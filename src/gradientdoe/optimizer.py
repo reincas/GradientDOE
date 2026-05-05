933b4616032898eaaae3b7c990c2d97e74a838d9
reincas <r.caspary@posteo.de>
2026-05-05 14:15:36 +0200

    Improved plotting

--- src/gradientdoe/optimizer.py
+++ src/gradientdoe/optimizer.py
@@ -220,10 +220,6 @@ class Optimizer:
     def step(self, height, method, count_s):
         """ Calculate H, P, and Ps for a given physical height profile illuminated by unit fields. """
 
-        print(torch.cuda.memory_summary(device=None, abbreviated=False))
-        torch.cuda.empty_cache()
-        print(torch.cuda.memory_summary(device=None, abbreviated=False))
-
         # Prepare height tensor
         if isinstance(height, torch.Tensor):
             height_tensor = height.to(self.device)
