2dba2d89f9f16d3d3e594f4506a5408159e4a783
reincas <r.caspary@posteo.de>
2026-05-05 15:13:15 +0200

    Introduced height_raw (again)

--- src/gradientdoe/optimizer.py
+++ src/gradientdoe/optimizer.py
@@ -171,7 +171,7 @@ class Optimizer:
         if isinstance(height, torch.Tensor):
             return torch.logit(height / self.h_max)
         x = height / self.h_max
-        return np.log(x / (1 - x + 1e-9))
+        return np.log(x / (1 - x - 1e-9))
 
     def interpolate_height(self, height, target_count):
         """ Tensor-based spectral interpolation of a height profile. """
