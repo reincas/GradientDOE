dd0aba4aa5e0fee5642eb868fd4903567d9a99cf
reincas <r.caspary@posteo.de>
2026-05-06 22:57:28 +0200

    Changed configuration

--- run/main.py
+++ run/main.py
@@ -75,7 +75,7 @@ EXPERIMENT = {
         "maxLoops": 1000000,
         "initialLearningRate": 0.05,
         "finalLearningRate": 0.05,
-        "maxHeightFactor": 2e-3,
+        "maxHeightFactor": 5e-3,
         "weightOrtho": 5.0,
         "expOrtho": 2,
         "weightEta": 50.0,
