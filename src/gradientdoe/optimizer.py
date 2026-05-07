256a184f6bf6d3d904d54e132e950617d0b74874
reincas <r.caspary@posteo.de>
2026-05-07 21:33:04 +0200

    Remove clip_height()

--- src/gradientdoe/optimizer.py
+++ src/gradientdoe/optimizer.py
@@ -301,15 +301,6 @@ class Optimizer:
         # Return results
         return P, Ps
 
-    def clip_height(self, height, fuzz, h_max=None):
-        if h_max is None:
-            h_max = self.h_max
-        if isinstance(height, torch.Tensor):
-            return soft_clip(height, h_max, fuzz * h_max)
-        height = torch.tensor(height, device=self.device, dtype=torch.float32)
-        height_clipped = self.clip_height(height, fuzz, h_max)
-        return height_clipped.detach().cpu().numpy()
-
     def run(self, height, learning_rate):
         assert isinstance(height, np.ndarray)
 
