669fa71a719891a79d761e90717fa0cc3e5cb6d5
reincas <r.caspary@posteo.de>
2026-05-07 20:34:54 +0200

    Changed configuration parameters

--- run/main.py
+++ run/main.py
@@ -90,7 +90,7 @@ EXPERIMENT = {
         "weightOrtho": 5.0,
         "expOrtho": 2,
         "weightEta": 50.0,
-        "weightGrad": 200.0,
+        "weightGrad": 50.0,
         "jitter": False,
         "ema": {
             "patience": 200,
