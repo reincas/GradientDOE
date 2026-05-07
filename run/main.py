80917bd61f1671fd76b99ecb6c38b98fbb7bc2f9
reincas <r.caspary@posteo.de>
2026-05-07 11:16:56 +0200

    Get result folder from command line

--- run/main.py
+++ run/main.py
@@ -96,12 +96,12 @@ EXPERIMENT = {
 }
 
 
-def init_logger(root):
+def init_logger(root_path):
     root = logging.getLogger()
     root.setLevel(logging.DEBUG)
     log_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-    file = root / "gradientdoe.log"
+    file = root_path / "gradientdoe.log"
     file_h = logging.FileHandler(file, mode="a")
     file_h.setFormatter(log_format)
     file_h.setLevel(logging.DEBUG)
